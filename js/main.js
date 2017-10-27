01095eafcf6335561903668d9210e26c0817a3c0
RahilModi <rmodi10@gmail.com>
2017-10-27 10:12:25 -0700

    updates

--- js/main.js
+++ js/main.js
@@ -335,58 +335,7 @@ function Timeline(cvs) {
     self.isOK = init();
 }
 new Timeline($('#cvs3').get(0)).toggle(true);
-//ajax form
-$(function() {
 
-    // Get the form.
-    var form = $('#ajax-contact');
-
-    // Get the messages div.
-    var formMessages = $('#form-messages');
-
-    // Set up an event listener for the contact form.
-    $(form).submit(function(e) {
-        // Stop the browser from submitting the form.
-        e.preventDefault();
-
-        // Serialize the form data.
-        var formData = $(form).serialize();
-
-        // Submit the form using AJAX.
-        $.ajax({
-            type: 'POST',
-            url: $(form).attr('action'),
-            data: formData
-        })
-            .done(function(response) {
-                // Make sure that the formMessages div has the 'success' class.
-                $(formMessages).removeClass('error');
-                $(formMessages).addClass('success');
-
-                // Set the message text.
-                $(formMessages).text(response);
-
-                // Clear the form.
-                $('#name').val('');
-                $('#email').val('');
-                $('#message').val('');
-            })
-            .fail(function(data) {
-                // Make sure that the formMessages div has the 'error' class.
-                $(formMessages).removeClass('success');
-                $(formMessages).addClass('error');
-
-                // Set the message text.
-                if (data.responseText !== '') {
-                    $(formMessages).text(data.responseText);
-                } else {
-                    $(formMessages).text('Oops! An error occured and your message could not be sent.');
-                }
-            });
-
-    });
-
-});
 $(document).ready(function () {
     $('.skill-icons').children('.active').each(function(i) {
         var row = $(this);
